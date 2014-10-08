b9bbef8f9f478ee8aeb13ae2c84c58d693a837e6
davidlandwehr <david@pppweb.dk>
2014-10-08 06:22:10 +0200

    Initial Commit

---
+++ BTLEDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BTLEDemo
+//
+//  Created by David Landwehr on 08/10/14.
+//  Copyright (c) 2014 David Landwehr. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ BTLEDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  BTLEDemo
+//
+//  Created by David Landwehr on 08/10/14.
+//  Copyright (c) 2014 David Landwehr. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
